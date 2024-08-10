d91a431495de9b23c0917722f893ce3d03d50bc0
tk226 <69638379+tk-engineer@users.noreply.github.com>
2024-08-10 23:52:44 +0000

    finish create model 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  # get 'post_images/new'
+  # get 'post_images/index'
+  # get 'post_images/show'
   devise_for :users
   # get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   root to: "homes#top"
+  
+  resources :post_images, only: [:new, :index, :show]
   
   get 'homes/about', as: "about"
 end
