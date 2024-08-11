0158ffeef5735da9aa6df54ef0a30b79d8c16f1b
tk226 <69638379+tk-engineer@users.noreply.github.com>
2024-08-11 22:08:51 +0000

    finish user function 3

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   get 'homes/about', as: "about"
 end
