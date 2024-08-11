a848ba291aaa87d2db4092b647cf3238d2869095
tk226 <69638379+tk-engineer@users.noreply.github.com>
2024-08-11 00:03:13 +0000

    finish create function

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   
   root to: "homes#top"
   
-  resources :post_images, only: [:new, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show]
   
   get 'homes/about', as: "about"
 end
