e8feeed33b50a9f3c6a12869821d6042d4f599df
tk226 <69638379+tk-engineer@users.noreply.github.com>
2024-08-14 15:12:32 +0000

    finish comment function

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,10 @@ Rails.application.routes.draw do
   
   root to: "homes#top"
   
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
+
   get 'homes/about', as: "about"
 end
